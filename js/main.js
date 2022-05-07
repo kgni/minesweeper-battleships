65b203c78b3721e6cc71de633ad7ec4f97f413e0
kgni <admin@mdia.dk>
2022-05-07 16:22:59 +0200

    add logic for populing board array, levels, bombs etc

--- js/main.js
+++ js/main.js
@@ -19,9 +19,10 @@
 // }
 
 class MineSweeper {
-	constructor(board, bombs) {
+	constructor(board, level) {
 		this.board = board;
-		this.bombs = bombs;
+		this.level = level;
+		this.bombs = 0;
 		this.bombFlags = 0;
 		this.isGameOver = false;
 	}
@@ -34,7 +35,10 @@ class MineSweeper {
 		// - rightclick place bombflag (update bombflag)
 	}
 	checkGameOver() {}
+
 	createBoard() {
+		this.board = Array(inputBoardSize.value ** 2).fill('');
+		this.bombs;
 		// get input value for boardsize
 		//  - generate array of arrays, and fill with empty string
 		//  - randomize bombplacements and insert into array positions
@@ -42,27 +46,77 @@ class MineSweeper {
 		//  - insert board into DOM.
 		// Generate
 	}
+	renderBoard() {}
 	reset() {}
 }
 
-const test = new MineSweeper(2);
-
 const btnGenerateBoard = document.querySelector('.btnGenerateBoard');
-btnGenerateBoard.addEventListener('click', (e) => {
-	console.log(e);
-});
-// function factorialize(num) {
-//     if (num <= 1)
-//         return 1;
-//     else {
-//         return (num * factorialize(num - 1));
-//     }
-//   }
-//   factorialize(5);
+//bombs get values
+// let bombs = document.getElementById('selectLevel').value;
 
-// 0! = 1
-// 1! = 1
-// 2! = 2 * 1
-// 3! = 3 * 2 * 1
-// 4! = 4 * 3 * 2 * 1
-// 5! = 5 * 4 * 3 * 2 * 1
+// console.log(bombs);
+
+//get board size define it
+// let boardSize = document.querySelector('.inputBoardSize').value;
+
+const newGame = new MineSweeper(0, '');
+
+function createMineSweeper() {
+	const level = document.getElementById('selectLevel').value;
+	const boardSize = document.querySelector('.inputBoardSize').value;
+	if (boardSize == Number(boardSize)) {
+		newGame.board = Array(Number(boardSize ** 2)).fill('');
+	} else {
+		alert('Board size has to be a number');
+		throw Error('Board size has to be a number');
+	}
+	// newGame.board = boardSize;
+	newGame.level = level;
+	amountOfBombs(newGame.level, newGame.board);
+	// newGame.startGame();
+	placeBombs(newGame.board, newGame.bombs);
+
+	console.log(newGame.board);
+	console.log(newGame.level);
+	console.log(newGame.bombs);
+}
+
+function amountOfBombs(level, boardArr) {
+	const board = boardArr.length;
+	switch (level) {
+		case 'easy':
+			newGame.bombs = Math.ceil(board / 10);
+			break;
+		case 'medium':
+			newGame.bombs = Math.ceil(board / 7);
+			break;
+		case 'hard':
+			newGame.bombs = Math.ceil(board / 4);
+			break;
+		case 'insane':
+			newGame.bombs = Math.ceil(board / 2);
+			break;
+		case 'nightmare':
+			newGame.bombs = board - 1;
+			break;
+	}
+}
+
+function placeBombs(array, bombs) {
+	while (bombs > 0) {
+		let num = Math.floor(Math.random() * array.length);
+		if (array[num] === '') {
+			array[num] = 'x';
+			bombs--;
+		}
+	}
+}
+
+btnGenerateBoard.addEventListener('click', createMineSweeper);
+
+//Button for testing purposes
+// document.querySelector('.test').addEventListener('click', tester);
+// function tester() {
+// 	const bombs = document.getElementById('selectLevel').value;
+// 	console.log(bombs);
+// }
